581252a5ced6762472f897f57f7fd1519d506958
amrsunja <amrsunja@gmail.com>
2026-05-14 21:00:33 +0200

    🍱 added new assets

---
+++ nour_app/lib/gen/assets.gen.dart
@@ -0,0 +1,330 @@
+// dart format width=80
+
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: deprecated_member_use,directives_ordering,implicit_dynamic_list_literal,unnecessary_import
+
+import 'package:flutter/widgets.dart';
+
+class $AssetsIconsGen {
+  const $AssetsIconsGen();
+
+  /// File path: assets/icons/home.svg
+  String get home => 'assets/icons/home.svg';
+
+  /// File path: assets/icons/impact.svg
+  String get impact => 'assets/icons/impact.svg';
+
+  /// File path: assets/icons/mute.svg
+  String get mute => 'assets/icons/mute.svg';
+
+  /// File path: assets/icons/refresh.svg
+  String get refresh => 'assets/icons/refresh.svg';
+
+  /// File path: assets/icons/source.svg
+  String get source => 'assets/icons/source.svg';
+
+  /// File path: assets/icons/tools.svg
+  String get tools => 'assets/icons/tools.svg';
+
+  /// File path: assets/icons/unmute.svg
+  String get unmute => 'assets/icons/unmute.svg';
+
+  /// List of all assets
+  List<String> get values => [
+    home,
+    impact,
+    mute,
+    refresh,
+    source,
+    tools,
+    unmute,
+  ];
+}
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/illustration_1.png
+  AssetGenImage get illustration1 =>
+      const AssetGenImage('assets/images/illustration_1.png');
+
+  /// File path: assets/images/illustration_10.png
+  AssetGenImage get illustration10 =>
+      const AssetGenImage('assets/images/illustration_10.png');
+
+  /// File path: assets/images/illustration_11.png
+  AssetGenImage get illustration11 =>
+      const AssetGenImage('assets/images/illustration_11.png');
+
+  /// File path: assets/images/illustration_12.png
+  AssetGenImage get illustration12 =>
+      const AssetGenImage('assets/images/illustration_12.png');
+
+  /// File path: assets/images/illustration_13.png
+  AssetGenImage get illustration13 =>
+      const AssetGenImage('assets/images/illustration_13.png');
+
+  /// File path: assets/images/illustration_14.png
+  AssetGenImage get illustration14 =>
+      const AssetGenImage('assets/images/illustration_14.png');
+
+  /// File path: assets/images/illustration_15.png
+  AssetGenImage get illustration15 =>
+      const AssetGenImage('assets/images/illustration_15.png');
+
+  /// File path: assets/images/illustration_16.png
+  AssetGenImage get illustration16 =>
+      const AssetGenImage('assets/images/illustration_16.png');
+
+  /// File path: assets/images/illustration_17.png
+  AssetGenImage get illustration17 =>
+      const AssetGenImage('assets/images/illustration_17.png');
+
+  /// File path: assets/images/illustration_18.png
+  AssetGenImage get illustration18 =>
+      const AssetGenImage('assets/images/illustration_18.png');
+
+  /// File path: assets/images/illustration_19.png
+  AssetGenImage get illustration19 =>
+      const AssetGenImage('assets/images/illustration_19.png');
+
+  /// File path: assets/images/illustration_2.png
+  AssetGenImage get illustration2 =>
+      const AssetGenImage('assets/images/illustration_2.png');
+
+  /// File path: assets/images/illustration_20.png
+  AssetGenImage get illustration20 =>
+      const AssetGenImage('assets/images/illustration_20.png');
+
+  /// File path: assets/images/illustration_21.png
+  AssetGenImage get illustration21 =>
+      const AssetGenImage('assets/images/illustration_21.png');
+
+  /// File path: assets/images/illustration_22.png
+  AssetGenImage get illustration22 =>
+      const AssetGenImage('assets/images/illustration_22.png');
+
+  /// File path: assets/images/illustration_23.png
+  AssetGenImage get illustration23 =>
+      const AssetGenImage('assets/images/illustration_23.png');
+
+  /// File path: assets/images/illustration_24.png
+  AssetGenImage get illustration24 =>
+      const AssetGenImage('assets/images/illustration_24.png');
+
+  /// File path: assets/images/illustration_25.png
+  AssetGenImage get illustration25 =>
+      const AssetGenImage('assets/images/illustration_25.png');
+
+  /// File path: assets/images/illustration_26.png
+  AssetGenImage get illustration26 =>
+      const AssetGenImage('assets/images/illustration_26.png');
+
+  /// File path: assets/images/illustration_27.png
+  AssetGenImage get illustration27 =>
+      const AssetGenImage('assets/images/illustration_27.png');
+
+  /// File path: assets/images/illustration_28.png
+  AssetGenImage get illustration28 =>
+      const AssetGenImage('assets/images/illustration_28.png');
+
+  /// File path: assets/images/illustration_29.png
+  AssetGenImage get illustration29 =>
+      const AssetGenImage('assets/images/illustration_29.png');
+
+  /// File path: assets/images/illustration_3.png
+  AssetGenImage get illustration3 =>
+      const AssetGenImage('assets/images/illustration_3.png');
+
+  /// File path: assets/images/illustration_30.png
+  AssetGenImage get illustration30 =>
+      const AssetGenImage('assets/images/illustration_30.png');
+
+  /// File path: assets/images/illustration_31.png
+  AssetGenImage get illustration31 =>
+      const AssetGenImage('assets/images/illustration_31.png');
+
+  /// File path: assets/images/illustration_32.png
+  AssetGenImage get illustration32 =>
+      const AssetGenImage('assets/images/illustration_32.png');
+
+  /// File path: assets/images/illustration_33.png
+  AssetGenImage get illustration33 =>
+      const AssetGenImage('assets/images/illustration_33.png');
+
+  /// File path: assets/images/illustration_34.png
+  AssetGenImage get illustration34 =>
+      const AssetGenImage('assets/images/illustration_34.png');
+
+  /// File path: assets/images/illustration_35.png
+  AssetGenImage get illustration35 =>
+      const AssetGenImage('assets/images/illustration_35.png');
+
+  /// File path: assets/images/illustration_4.png
+  AssetGenImage get illustration4 =>
+      const AssetGenImage('assets/images/illustration_4.png');
+
+  /// File path: assets/images/illustration_5.png
+  AssetGenImage get illustration5 =>
+      const AssetGenImage('assets/images/illustration_5.png');
+
+  /// File path: assets/images/illustration_6.png
+  AssetGenImage get illustration6 =>
+      const AssetGenImage('assets/images/illustration_6.png');
+
+  /// File path: assets/images/illustration_7.png
+  AssetGenImage get illustration7 =>
+      const AssetGenImage('assets/images/illustration_7.png');
+
+  /// File path: assets/images/illustration_8.png
+  AssetGenImage get illustration8 =>
+      const AssetGenImage('assets/images/illustration_8.png');
+
+  /// File path: assets/images/illustration_9.png
+  AssetGenImage get illustration9 =>
+      const AssetGenImage('assets/images/illustration_9.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+    illustration1,
+    illustration10,
+    illustration11,
+    illustration12,
+    illustration13,
+    illustration14,
+    illustration15,
+    illustration16,
+    illustration17,
+    illustration18,
+    illustration19,
+    illustration2,
+    illustration20,
+    illustration21,
+    illustration22,
+    illustration23,
+    illustration24,
+    illustration25,
+    illustration26,
+    illustration27,
+    illustration28,
+    illustration29,
+    illustration3,
+    illustration30,
+    illustration31,
+    illustration32,
+    illustration33,
+    illustration34,
+    illustration35,
+    illustration4,
+    illustration5,
+    illustration6,
+    illustration7,
+    illustration8,
+    illustration9,
+  ];
+}
+
+class Assets {
+  const Assets._();
+
+  static const String aEnv = '.env';
+  static const $AssetsIconsGen icons = $AssetsIconsGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+
+  /// List of all assets
+  static List<String> get values => [aEnv];
+}
+
+class AssetGenImage {
+  const AssetGenImage(
+    this._assetName, {
+    this.size,
+    this.flavors = const {},
+    this.animation,
+  });
+
+  final String _assetName;
+
+  final Size? size;
+  final Set<String> flavors;
+  final AssetGenImageAnimation? animation;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = true,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.medium,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({AssetBundle? bundle, String? package}) {
+    return AssetImage(_assetName, bundle: bundle, package: package);
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
+
+class AssetGenImageAnimation {
+  const AssetGenImageAnimation({
+    required this.isAnimation,
+    required this.duration,
+    required this.frames,
+  });
+
+  final bool isAnimation;
+  final Duration duration;
+  final int frames;
+}
